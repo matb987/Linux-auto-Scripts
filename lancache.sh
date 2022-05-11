f004541f8a14ba31a737eacea3056f4b391bd47d
matb987 <admin@mbdev.org>
2022-05-11 16:32:06 +0100

    Create lancache.sh

---
+++ lancache.sh
@@ -0,0 +1,8 @@
+apt=get update apt-get dist-upgrade
+apt-get install git
+apt-get install docker-compose
+git clone https://github.com/lancachenet/docker-compose/ lancache
+cd lancache
+nano .env
+pause
+docker-compose up -d
